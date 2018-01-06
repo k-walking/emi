53dbca76d3dfd50f36ad8e9fa88d8d7719a3a82a
mikrohash <iota@mikrohash.de>
2018-01-06 16:29:32 +0100

    added DiagramBuilder & Persistence classes

---
+++ app/src/main/java/de/logcat/viktor/fittnessapp1/DiagramBuilder.java
@@ -0,0 +1,22 @@
+package de.logcat.viktor.fittnessapp1;
+
+import java.awt.Image;
+
+public class DiagramBuilder {
+
+    public static Image buildRunningMap(TimeBindedCoordinate[] coords) {
+        return null; // TODO
+    }
+
+    public static Image buildRunningSpeedDiagram(TimeBindedCoordinate[] coords) {
+        return null; // TODO
+    }
+
+    public static Image buildDurationProgressDiagram(Execution[] execs) {
+        return null; // TODO
+    }
+
+    public static Image buildQuantityProgressDiagram(Execution[] execs) {
+        return null; // TODO
+    }
+}
---
+++ app/src/main/java/de/logcat/viktor/fittnessapp1/Persistence.java
@@ -0,0 +1,44 @@
+package de.logcat.viktor.fittnessapp1;
+
+import java.util.ArrayList;
+
+public class Persistence {
+
+    // === save ===
+
+    public static void save(Routine routine) {
+        // TODO
+    }
+
+    public static void save(Execution execution) {
+        // TODO
+    }
+
+    public static void saveProgressDiagrams(Category category, Image durationProgressDiagram, Image quantityProgressDiagram) {
+        // TODO
+    }
+
+
+
+    // === load ====
+
+    public static ArrayList<Routine> loadRoutines() {
+        return null; // TODO
+    }
+
+    public static ArrayList<Execution> loadExecutions() {
+        return null; // TODO
+    }
+
+
+
+    // === delete ===
+
+    public static void deleteRoutine(int id) {
+        // TODO
+    }
+
+    public static void deleteTarget(int id) {
+        // TODO
+    }
+}
